bdd0982f146ee9d9db3e91aa741da10d34ad30db
arivuwallet-sketch <arivuwallet@gmail.com>
2026-09-20 10:51:39 +0530

    Mid-level gameplay: add integrations/unreal/Source/GenesisGameplay/Public/GenesisGameplayTags.h

---
+++ integrations/unreal/Source/GenesisGameplay/Public/GenesisGameplayTags.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "GameplayTagContainer.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Primary);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Interact);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Movement_DoubleJump);
