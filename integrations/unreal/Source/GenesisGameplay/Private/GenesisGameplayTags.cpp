0f177990fe905484a8790745220ca328651ade74
arivuwallet-sketch <arivuwallet@gmail.com>
2026-09-20 10:51:41 +0530

    Mid-level gameplay: add integrations/unreal/Source/GenesisGameplay/Private/GenesisGameplayTags.cpp

---
+++ integrations/unreal/Source/GenesisGameplay/Private/GenesisGameplayTags.cpp
@@ -0,0 +1,13 @@
+#include "GenesisGameplayTags.h"
+
+UE_DEFINE_GAMEPLAY_TAG(
+    TAG_Ability_Primary,
+    "Ability.Primary");
+
+UE_DEFINE_GAMEPLAY_TAG(
+    TAG_Ability_Interact,
+    "Ability.Interact");
+
+UE_DEFINE_GAMEPLAY_TAG(
+    TAG_Ability_Movement_DoubleJump,
+    "Ability.Movement.DoubleJump");
